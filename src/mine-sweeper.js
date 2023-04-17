99b93d511bba8cbf63a1a116e2aa58b477d0082e
dongl1n <102480011+dongl1n@users.noreply.github.com>
2023-04-17 22:03:42 +0300

    feat: add func

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,81 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrix(arr, n, m){
+  let matrix=[]
+  for(let i=0, k = 0; i<n; i++){
+    matrix[i]=[];
+    for(let j=0; j<m; j++){
+      matrix[i][j]=arr[k++];
+    }
+  }
+  return matrix;
+}
+
+function minesweeper(matrix){
+  let count=0, arr=[];
+  for(let i=0; i<matrix.length; i++){
+    for(let j=0; j<matrix[i].length; j++){
+      count=0;
+      if(j===0 && i===0){ //Верхний крайний слева
+        if(matrix[i+1][j]) count++;
+        if(matrix[i][j+1]) count++;
+        if(matrix[i+1][j+1]) count++;
+      }
+      else if(j===matrix[i].length-1 && i===0){ //Верхний крайний справа
+        if(matrix[i+1][j]) count++;
+        if(matrix[i][j-1]) count++;  
+        if(matrix[i+1][j-1]) count++;
+      }
+      else if(i===0){ //Верхнй центральный
+        if(matrix[i+1][j]) count++;
+        if(matrix[i][j-1]) count++;   
+        if(matrix[i][j+1]) count++;
+      }
+      else if(j===0 && i===matrix.length-1){ //Нижний крайний левый
+        if(matrix[i-1][j]) count++;
+        if(matrix[i][j+1]) count++;
+        if(matrix[i-1][j+1]) count++;
+      }
+      else if(j===matrix[i].length-1 && i===matrix.length-1){ //Нижний крайний правый
+        if(matrix[i-1][j]) count++;
+        if(matrix[i][j-1]) count++;  
+        if(matrix[i-1][j-1]) count++;
+      }
+      else if(i===matrix.length-1){ //Нижний центральный
+        if(matrix[i-1][j]) count++;
+        if(matrix[i][j-1]) count++;   
+        if(matrix[i][j+1]) count++;
+      }
+      else if(j===0){ //Левый центральный
+        if(matrix[i-1][j]) count++;
+        if(matrix[i][j+1]) count++;   
+        if(matrix[i+1][j]) count++;
+        if(matrix[i-1][j+1]) count++;
+        if(matrix[i+1][j+1]) count++;
+      }
+      else if(j===matrix[i].length-1){ //Правый центральный
+        if(matrix[i-1][j-1]) count++;
+        if(matrix[i-1][j]) count++;   
+        if(matrix[i][j-1]) count++;
+        if(matrix[i+1][j-1]) count++;
+        if(matrix[i+1][j]) count++;
+        
+      }
+      else{
+        if(matrix[i-1][j-1]) count++;
+        if(matrix[i-1][j]) count++;
+        if(matrix[i-1][j+1]) count++;
+        if(matrix[i][j-1]) count++;
+        if(matrix[i][j+1]) count++;
+        if(matrix[i+1][j-1]) count++;
+        if(matrix[i+1][j]) count++;
+        if(matrix[i+1][j+1]) count++;
+      }
+      arr.push(count);
+    }
+  }
+  return getMatrix(arr, matrix.length, matrix[0].length);
 }
 
 module.exports = {
