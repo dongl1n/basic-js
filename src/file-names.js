b8ff1bfcc8672aa903ff3eec3e67f9460e4945ab
dongl1n <102480011+dongl1n@users.noreply.github.com>
2023-04-17 23:07:18 +0300

    feat: add func

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function counter(arr, index, value){
+  let count=0;
+  for(let i=0; i<index; i++){
+    if(value===arr[i]) count++;
+  }  
+  if(!count) return '';
+  return count;
+}
+
+
+function renameFiles(arr){
+  let newArr=[]
+  for(let i=0; i<arr.length; i++){
+    if(!newArr.includes(arr[i])) newArr.push(arr[i]);
+    else{
+      let count=counter(arr, i, arr[i]);
+      if(!count) newArr.push(arr[i]);
+      else newArr.push(arr[i]+'('+counter(arr, i, arr[i])+')');  
+    }
+  }
+  console.log(newArr)
+  let d=newArr.filter((number, index, numbers) => {
+  return numbers.indexOf(number) !== index;})
+  console.log(newArr)
+  console.log('___')
+  if(d.length!==0) return renameFiles(newArr);
+  return newArr;
 }
 
 module.exports = {
