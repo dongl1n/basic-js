c645f0376325a12f3ab6418ed801de81a422eb89
dongl1n <102480011+dongl1n@users.noreply.github.com>
2023-04-17 22:00:50 +0300

    feat: add func

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(arr){
+  let obj={}, str;
+  for(let i=0; i<arr.length; i++){
+    str='';
+    let buf=arr[i].split('.');
+    str='.'.concat(buf.reverse().join('.'));
+    for(key in obj){
+      if(key===('.'.concat(buf.reverse().join('.')))) str='';
+    }
+    if(str==='') break;
+    for(key2 in obj){
+      if(str.includes(key2)) obj[key2]+=1; 
+      if(key2.includes(str)){ console.log('a')}
+    }
+    if (typeof obj[str] !== "undefined") {
+        obj[str]+=1;  
+    }else{
+        obj[str]=1;
+    }
+  }
+  for(let i=0; i<arr.length; i++){
+    if(arr[i].match(/.[A-Za-z0-9_-]$/)){
+      str=arr[i].match(/.[A-Za-z0-9_-]{1,}$/)[0];
+      if (typeof obj[str] !== "undefined") {
+        obj[str]+=1;  
+    }else{
+        obj[str]=1;
+    }
+  }
+  }
+  return obj;
 }
 
 module.exports = {
