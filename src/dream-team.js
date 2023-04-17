290ce79de1cc4a3afb63d761eb150ae5d23394cb
dongl1n <102480011+dongl1n@users.noreply.github.com>
2023-04-17 22:00:12 +0300

    feat: add func

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const flatSingle = arr => [].concat(...arr);
+const flatAll = arr => {
+  for(key in arr){
+    if(Array.isArray(arr[key])){
+      arr=flatSingle(arr);
+      flatAll(arr);
+    }
+  }
+  return arr;
+}
+
+function createDreamTeam(arr){
+  if(!Array.isArray(arr)) return 0;
+  let str=[];
+  for(key in arr){
+    if(typeof arr[key] === 'string'){
+      arr[key]=arr[key].toString().trim();
+      str.push(arr[key][0].toUpperCase());  
+    }
+  }
+  if(str.length<2) return 0;
+  return str.sort().join('');
 }
 
 module.exports = {
